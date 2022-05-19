9c45d61e47c39f54f1dd59d341dbdd2411429a92
George Tang <gtang.gt@gmail.com>
2022-05-19 10:21:11 -0700

    cssAnimations study

--- drawGraph/drawCircle.js
+++ drawGraph/drawCircle.js
@@ -1,3 +1,7 @@
-export function drawCircle () {
-    console.log("drawCircle")
+export function drawCircle (canvas) {
+    const ctx = canvas.getContext('2d');
+
+    ctx.fillRect(25, 25, 100, 100);
+    ctx.clearRect(45, 45, 60, 60);
+    ctx.strokeRect(50, 50, 50, 50);
 }
--- drawGraph/drawGraph.js
+++ drawGraph/drawGraph.js
@@ -1,13 +1,9 @@
 import { drawCircle } from "./drawCircle.js";
 
-const div = document.createElement("body");
 const canvas = document.createElement("canvas");
 
 canvas.style = "width: 100%; height: 100%; background: blue; margin: 20px;";
 
-drawCircle();       
+drawCircle(canvas);
 
-div.appendChild(canvas);
-
-
-document.body = div;
+document.body.appendChild(canvas);
